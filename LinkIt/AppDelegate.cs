de9b82e316a93f61315c8b7a7ad92d7cde6153c6
Mikhail.udot <Mikhail.udot@venafi.com>
2018-03-17 00:58:01 +0300

    Status bar item added

--- LinkIt/AppDelegate.cs
+++ LinkIt/AppDelegate.cs
@@ -6,6 +6,8 @@ namespace LinkIt
     [Register("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
     {
+        private NSStatusItem _barItem;
+
         public AppDelegate()
         {
         }
@@ -13,6 +15,8 @@ namespace LinkIt
         public override void DidFinishLaunching(NSNotification notification)
         {
             // Insert code here to initialize your application
+            _barItem =  NSStatusBar.SystemStatusBar.CreateStatusItem(NSStatusItemLength.Variable);
+            _barItem.Title = "LINK IT!";
         }
 
         public override void WillTerminate(NSNotification notification)
